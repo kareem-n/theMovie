7f4e2642b6cabcec58b9267c8659585a82a022e0
kareem-n <kareemnasser1369@gmail.com>
2022-06-26 16:59:03 +0200

    lightBox added when item clicked

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ $("#close").click(function () {
 
 // api
 let cont = [];
+let lightBoxItem = [];
 
 async function api(typp) {
   var response = await fetch(
@@ -25,7 +26,7 @@ async function api(typp) {
   display(cont);
 }
 
-async function apiT(typp) {
+async function apiT() {
   var response = await fetch(
     `https://api.themoviedb.org/3/trending/movie/day?api_key=eba8b9a7199efdcb0ca1f96879b83c44`
   );
@@ -42,6 +43,46 @@ async function apiWord(x) {
   cont = final.results;
   display(cont);
 }
+async function apiMovieDetails(idd) {
+  let response = await fetch(
+    `https://api.themoviedb.org/3/movie/${idd}?api_key=eba8b9a7199efdcb0ca1f96879b83c44&language=en-US`
+  );
+  var final = await response.json();
+  lightBoxDiplay(final);
+}
+
+function lightBoxDiplay(data) {
+  $(".lightItem").css(
+    "backgroundImage",
+    `url(https://image.tmdb.org/t/p/w500${data.backdrop_path})`
+  );
+  let temp1 = ``;
+  for (let i = 0; i < data.genres.length; i++) {
+    temp1 += `
+        <span class="me-2">${data.genres[i].name}</span>
+      `;
+  }
+  let temp = `
+
+  <div class="col-lg-5">
+    <img src="https://image.tmdb.org/t/p/w500${data.poster_path}" class="w-100 rounded-3" alt="" />
+  </div>
+  <div class="col-lg-7 text-white pt-3">
+  <h2>${data.original_title}</h2>
+  <p>${data.overview}</p>
+  <div class="mb-2">${temp1}</div>
+  <div class="d-flex flex-column">
+    <p >Country : ${data.production_countries[0].iso_3166_1}</p>
+    <p >Release Date : ${data.release_date}</p>
+    <p >Release Date : ${data.spoken_languages[0].name}</p>
+    <p>Vote Avg : ${data.vote_average}</p>
+  <p>Vote Count : ${data.vote_count}</p>
+  </div>
+  </div>
+  `;
+  document.querySelector("#lightBox_content").innerHTML = temp;
+  $("#lightBox_content").slideDown(1000);
+}
 
 let word = document.querySelector("#word");
 $("#word").keyup(function () {
@@ -49,13 +90,11 @@ $("#word").keyup(function () {
 });
 let seko = document.querySelector("#seko");
 $("#seko").keyup(function () {
-  if( seko.value == "" ){
-    $('#search_content').html('')  ; 
-    console.log(000);
-  }else{
-    displaySearch(seko.value)
+  if (seko.value == "") {
+    $("#search_content").html("");
+  } else {
+    displaySearch(seko.value);
   }
-  
 });
 
 (function () {
@@ -73,8 +112,8 @@ function display(x) {
   var temp = ``;
   for (let i = 0; i < x.length; i++) {
     temp += `
-    <div class="col-lg-4 col-md-6">
-    <div class="itemmm rounded-3 position-relative overflow-hidden">
+    <div  class="col-lg-4 col-md-6">
+    <div index="${i}" class="itemmm rounded-3 position-relative overflow-hidden">
         <img src="https://image.tmdb.org/t/p/w500${x[i].poster_path}" class="w-100" alt="">
         <div class="caption p-4">
             <h3>${x[i].original_title}</h3>
@@ -87,16 +126,25 @@ function display(x) {
         `;
   }
   document.querySelector("#content").innerHTML = temp;
+
+  $(".itemmm").click(function () {
+    $("#lightBox_content").css("display", "none");
+    console.log(11);
+    let indexx = Number($(this).attr("index"));
+    apiMovieDetails(cont[indexx].id);
+    $("#lightBox").css("display", "flex");
+  });
 }
 
 function displaySearch(x) {
-  
   temp = ``;
   for (var i = 0; i < cont.length; i++) {
-    if (cont[i].original_title.toLowerCase().includes(x.toLowerCase()) == true) {
+    if (
+      cont[i].original_title.toLowerCase().includes(x.toLowerCase()) == true
+    ) {
       temp += `
             <div class="col-lg-4 col-md-6">
-            <div class="itemmm rounded-3 position-relative overflow-hidden">
+            <div index="${i}" class="itemmm rounded-3 position-relative overflow-hidden">
                 <img src="https://image.tmdb.org/t/p/w500${cont[i].poster_path}" class="w-100" alt="">
                 <div class="caption p-4">
                     <h3>${cont[i].original_title}</h3>
@@ -110,6 +158,12 @@ function displaySearch(x) {
     }
   }
   document.querySelector("#search_content").innerHTML = temp;
+  $(".itemmm").click(function () {
+    $("#lightBox_content").css("display", "none");
+    let indexx = Number($(this).attr("index"));
+    apiMovieDetails(cont[indexx].id);
+    $("#lightBox").css("display", "flex");
+  });
 }
 
 let name1 = document.querySelector("#name");
@@ -219,6 +273,12 @@ function repassCheck() {
     }
   });
 }
+
+$("#exit i").click(function () {
+  $("#lightBox").css("display", "none");
+  $("#lightBox_content").html("");
+  $("#lightItem").css("backgroundImage", "url()");
+});
 
 nameCheack();
 ageCheck();
